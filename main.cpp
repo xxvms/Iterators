bc5248e941342d707744c1a3d783bd0c0aef0eb8
xxvms <tom@pcservicegroup.co.uk>
2017-12-02 00:43:57 +0000

    updates

--- main.cpp
+++ main.cpp
@@ -1,5 +1,25 @@
-#include <iostream>
-int main() {
-  std::cout << "Hello, World!" << std::endl;
-  return 0;
-}
+  #include <vector>
+  #include <algorithm>
+  #include <numeric>
+  #include <iterator>
+  #include <deque>
+  #include <array>
+
+  int main() {
+
+    std::vector<int> v1(10);
+    std::fill(std::begin(v1), std::end(v1), 1);
+    std::fill_n(std::begin(v1), 6, 2);
+    std::iota(std::begin(v1), std::end(v1), 1);
+
+    std::vector<int> v2;
+    std::fill_n(std::back_inserter(v2), 6, 2);
+    std::generate_n(std::back_inserter(v2), 10, [n = 0]()mutable{ return n++;});
+
+    std::deque<int> q3;
+    std::fill_n(std::front_inserter(q3), 6, 2);
+    std::generate_n(std::front_inserter(q3), 10, [n= 0]()mutable{ return n++;});
+
+
+    return 0;
+  }
